60d320cf8f66267ac5aec71af7053d48c65b9072
PatrickEngelbert2 <patrickengelbert2@gmail.com>
2022-05-31 15:13:02 -0500

    add model functionality for items

--- public/react/components/Items.js
+++ public/react/components/Items.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React,{useState} from 'react';
+
 
 export function Items (props) {
 
-  const handleClick = () => {
-    
-  }
+  const [nameClicked,setNameClicked] = useState(false)
+  
+  const itemData = (
+    <div>
+      <p>Hello {props.items.description}</p>
+      <p>{props.items.category}</p>
+      <img src={props.items.image} alt={props.items.name} />
+      <p>{props.items.price}</p>
+    </div>
+  )
 
   return <>
-    <h3><button onClick={handleClick}>{props.items.name}</button></h3>
-    {/* <img src={props.items.image} alt={props.items.name} /> */}
+    <h3><button onClick={()=>{setNameClicked(!nameClicked)}}>Button {props.items.name}</button></h3>
+    { itemData && nameClicked}
   </>
-} 
+}
+
 	
