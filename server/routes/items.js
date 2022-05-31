98ffc8e723d97016d34181290c8c155596bc8021
PatrickEngelbert2 <patrickengelbert2@gmail.com>
2022-05-31 16:10:24 -0500

    Added back-end functionality to make a get request by id of item

--- server/routes/items.js
+++ server/routes/items.js
@@ -2,7 +2,13 @@ const express = require("express");
 const router = express.Router();
 const { Item } = require("../models");
 
-// GET /item
+// Get a single item by ID
+router.get("/:id", async (req, res, next) => {
+  const item = await Item.findByPk(req.params.id);
+  res.send(item);
+});
+
+// GET /item -- get all items
 router.get("/", async (req, res, next) => {
   try {
     const items = await Item.findAll();
