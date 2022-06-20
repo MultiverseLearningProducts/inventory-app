67a0108e7ab24d52cdc74a98fd557f34bd08f7a4
Rose (Ariel) <ariel.bowen@aol.com>
2022-06-20 12:45:52 -0400

    Item card and simple css added

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -3,8 +3,19 @@ import React from 'react';
 export const Item = (props) => {
 
   return <>
-    <h3>{props.item.title}</h3>
-    <img src={props.item.image} alt={props.item.title} />
+
+
+    <div className='itemCard'>
+
+      <h3 className='item-title'>{props.item.title}</h3>
+
+      <img src={props.item.image} alt={props.item.title} />
+
+      <p className='item-description'>{props.item.description}</p>
+
+    </div>
+      
   </>
+  
 } 
 	
