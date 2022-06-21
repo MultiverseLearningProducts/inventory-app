56e4f09bb28c846da417a3cc1311b7cb8dbb7100
holliday91 <joshua_holliday@mckinsey.com>
2022-06-21 19:33:50 -0400

    add and delete function updated

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import apiURL from '../api';
 
 export const Item = (props) => {
 
+  async function deleteitem(itemId){
+		try {
+			await fetch(`${apiURL}/items/${itemId}`,  {
+			  method: 'DELETE',
+			 	headers: {
+			   'Content-Type': 'application/json',
+			   },
+		   	});
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
   return <>
-
+  
 
     <div className='itemCard'>
 
@@ -12,10 +25,12 @@ export const Item = (props) => {
       <img src={props.item.image} alt={props.item.title} />
 
       <p className='item-description'>{props.item.description}</p>
-
+      <button onClick={()=>deleteitem(props.item.id)}>delete item</button>
     </div>
-      
+ 
   </>
   
 } 
-	
+
+//1. delete button that will invoke the delete fetch request, passing in the id
+//2. fetch request
--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Item } from './Item';
+import { ItemForm } from './ItemForm';
 
-export const ItemsList = ({items}) => {
+export const ItemsList = ({items, setitem, item, addItem}) => {
 	return <div className='allItems'>
+		{/* button to add item */}
+		<button onClick={() => addItem(item)}>Add item </button>
+		<ItemForm items={items} setitem={setitem} item={item} addItem={addItem}/>
+		
 		{
 			items.map((item, idx) => {
 				return <Item item={item} key={idx} />
 			})
 		}
+
 	</div>
 } 
