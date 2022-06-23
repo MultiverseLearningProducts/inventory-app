f0e907d392087dff281e7164b9bd37f48138687c
holliday91 <joshua_holliday@mckinsey.com>
2022-06-23 15:59:52 -0400

    updateform2

--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import { Item } from './Item';
-<<<<<<< HEAD
 import { ItemForm } from './ItemForm';
 import { UpdateForm } from './UpdateForm';
-=======
-// import { ItemForm } from './ItemForm';
->>>>>>> 3c3bd690b3a3ef3733b21e0b01a8ca48e6c08b2e
 
 export const ItemsList = ({items, setitem, item, addItem}) => {
 	return <div className='allItems'>
