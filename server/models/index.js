cec0572aa790d662dc80bffb4959c5adcfcec3ee
tahar ainouz <ainouzusa@gmail.com>
2022-09-16 18:51:57 +0100

    all-items

--- server/models/index.js
+++ server/models/index.js
@@ -1,10 +1,6 @@
 const { Sequelize } = require("sequelize");
 const { sequelize } = require("../db");
 
-// const Sauce = sequelize.define("sauces", {
-//   name: Sequelize.STRING,
-//   image: Sequelize.STRING,
-// });
 
 const Item = sequelize.define("items", {
   title: Sequelize.STRING,
