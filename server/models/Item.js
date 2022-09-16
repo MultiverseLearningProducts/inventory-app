85e65b435115dfedeb81213a6f48c167dabc3d1e
bryan99pham <bryan99pham@gmail.com>
2022-09-16 13:10:29 -0500

    created Item model

---
+++ server/models/Item.js
@@ -0,0 +1,12 @@
+const Sequelize = require("sequelize");
+const {sequelize} = require("../db");
+
+const Item = sequelize.define("items", {
+    title: Sequelize.STRING,
+    description: Sequelize.STRING,
+    price: Sequelize.NUMBER,
+    category: Sequelize.STRING,
+    image: Sequelize.STRING
+})
+
+module.exports = {Item};
