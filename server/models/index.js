966806b74db15aac32a8f1d4c3e564c3ef9672dc
PatrickB001 <patrickborgella1@gmail.com>
2022-09-16 12:19:46 -0500

    Creates the Item Model and places it in the seed file to create data

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,20 @@
-const {Sequelize} = require('sequelize')
-const {sequelize} = require('../db')
+const { Sequelize } = require("sequelize");
+const { sequelize } = require("../db");
 
-const Sauce = sequelize.define("sauces", {
-  name: Sequelize.STRING,
+// const Sauce = sequelize.define("sauces", {
+//   name: Sequelize.STRING,
+//   image: Sequelize.STRING,
+// });
+
+const Item = sequelize.define("items", {
+  title: Sequelize.STRING,
+  price: Sequelize.DECIMAL,
+  description: Sequelize.STRING,
+  category: Sequelize.STRING,
   image: Sequelize.STRING,
 });
 
 module.exports = {
   db: sequelize,
-  Sauce,
+  Item,
 };
