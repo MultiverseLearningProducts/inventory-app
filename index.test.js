f9ddab424fc88741aca73ffadf93f245719c18ad
tahar ainouz <ainouzusa@gmail.com>
2022-09-19 18:51:12 +0100

    test-for-all-items-and-one-item

--- index.test.js
+++ index.test.js
@@ -1,5 +1,9 @@
 const { sequelize } = require("./server/db");
-const {Item} = require('./server/models')
+const {Item} = require('./server/models/index')
+//import React from 'react';
+//import { render, fireEvent } from '@testing-library/react';
+
+import 'regenerator-runtime/runtime';
 
 describe('Testing', () => {
     beforeAll(async () => {
@@ -8,7 +12,16 @@ describe('Testing', () => {
     })
     test('Testing find all', async () => {
         //Testing find all
-        
+        const creatItems = await Item.create({
+            title:'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+            price:109.95,
+            description:"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            category:"men's clothing",
+            image:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+
+
+        },
+        )
         const foundAllItems = await Item.findAll();
         expect(foundAllItems[0].title).toEqual("Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops")
         expect(foundAllItems[0].price).toEqual(109.95)
@@ -19,8 +32,20 @@ describe('Testing', () => {
     })
     test('Testing by item', async () => {
         //Testing find by items
-        const foundOnlyItem= await Item.findByPk(id);
-        expect(foundOnlyItem.length).toBe(1);
+        const creatItemsByid = await Item.create({
+            
+            title:'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops',
+            price:109.95,
+            description:"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            category:"men's clothing",
+            image:"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+
+
+        },
+       
+    )
+        const foundOnlyItem= await Item.findByPk(1);
+        expect(foundOnlyItem.price).toEqual(109.95);
     })
 
 })
