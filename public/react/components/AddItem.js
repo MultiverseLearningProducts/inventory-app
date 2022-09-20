f6c4dbb46efdc450fa89a8eaa35984f3c5c127ec
bryan99pham <bryan99pham@gmail.com>
2022-09-20 12:36:20 -0500

    finished tier 2

--- public/react/components/AddItem.js
+++ public/react/components/AddItem.js
@@ -7,30 +7,46 @@ const [title, setTitle] = useState('');
 const [price, setPrice] = useState(null);
 const [description, setDescription] = useState('');
 const [category, setCategory] = useState('');
-const [itemImage, setItemImage] = useState('');   
+const [image, setImage] = useState('');   
 
 
-const handleSubmit = (ev) => {
-    const url = `${apiURL}/items`
-    const options = {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(
-            {title,price,description,category,itemImage}
-        )
-    }
-    fetch(url,options)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    setIsAddingItem(false);
-    props.item({});
-}
+// const handleSubmit = (ev) => {
+//     const url = `${apiURL}/items`
+//     const options = {
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(
+//             {title,price,description,category,itemImage}
+//         )
+//     }
+//     fetch(url,options)
+//     .then(response => response.json())
+//     .then(data => console.log(data))
+//     setIsAddingItem(false);
+//     props.item({});
+// }
+
+async function handleSubmit(ev) {
+    //event.preventDefault();
+    const response = await fetch(`${apiURL}/items/`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {title, price, description, category, image}
+      )
+    });
+
+    const data = await response.json();
+    setIsAddingItem(null);
+  }
     
     return  ( <>
     <h2>Add an Item</h2>
-     <form onSubmit={handleSubmit}>
+     <form>
     <label>
         <p>
         <input type="text" name="title" placeholder="Title" value={ title } onChange={ev => setTitle(ev.target.value)} /> 
@@ -45,11 +61,11 @@ const handleSubmit = (ev) => {
         <input type="text" name="category" placeholder="Category" value={ category } onChange={ev => setCategory(ev.target.value)}/> 
         </p>
         <p>
-        <input type="text" name="itemImage"  placeholder="Image" value={ itemImage } onChange={ev => setItemImage(ev.target.value)}/>
+        <input type="text" name="image"  placeholder="Image" value={ image } onChange={ev => setImage(ev.target.value)}/>
         </p> 
     </label>
     </form>
-    <button type="submit">Add My Item!</button>
+    <button type="submit" onClick={handleSubmit}>Add My Item!</button>
     <button onClick={() => setIsAddingItem(false)}>Back to Main Page</button>
     </>
     );
