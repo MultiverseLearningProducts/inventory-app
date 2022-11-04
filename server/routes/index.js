a272534eee6218770c2fbbf5c6494ed679863fb6
Emily Chieco <emilychieco@google.com>
2022-11-04 18:36:08 -0400

    Add express routes/GET request to get all items

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 // different model routers
-router.use('/sauces', require('./sauces'));
+
+//starter code below
+//router.use('/sauces', require('./sauces'));
+
+router.use('/items', require('./items'));
 
 module.exports = router;
