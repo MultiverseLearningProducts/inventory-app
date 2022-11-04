cd37ac3b27dcdaead52cfdbdd9080ad840a57155
emigoh <emi.goh2011@gmail.com>
2022-11-04 15:32:54 -0400

    changed name to title in models/Item, updated seed file to take in items data -- test in postman and is working

--- server/models/Item.js
+++ server/models/Item.js
@@ -4,7 +4,7 @@ const {sequelize} = require('../db')
 
 //defining the Item model
 const Item = sequelize.define('item', {
-    name: Sequelize.STRING,
+    title: Sequelize.STRING,
     description: Sequelize.STRING,
     price: Sequelize.INTEGER,
     category: Sequelize.STRING,
