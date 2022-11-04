621c04628421b02893f5ac500f4a08745db430e4
Brynner <bventura03@hotmail.com>
2022-11-04 15:32:27 -0400

    Models and routes for Items are completed

--- server/controllers/ItemControllers.js
+++ server/controllers/ItemControllers.js
@@ -1,9 +1,48 @@
 const { Item } = require('../models/index')
+const { post } = require('../routes')
 
 const GetItems = async (req, res) => {
     res.send(await Item.findAll())
 }
 
+const GetItemById = async (req, res) => {
+    const item = await Item.findByPk(req.params.id)
+    res.send(item)
+}
+
+const CreateItem = async (req, res) => {
+    let postBody = {
+        ...req.body
+    }
+    await Item.create(postBody)
+    res.send(await Item.findAll())
+}
+
+const UpdateItem = async (req, res) => {
+
+    let itemId = parseInt(req.params.id)
+
+    await Item.update(req.body, {
+        where: {id: itemId},
+        returning: true
+    })
+    res.send( await Item.findByPk(itemId))
+}
+
+const DeleteItem = async (req, res) => {
+
+    let itemId = parseInt(req.params.id)
+
+    await Item.destroy({
+        where: {id: itemId}
+    })
+    res.send({message: `Deleted item with the if of ${itemId}`})
+}
+
 module.exports = {
-    GetItems
+    GetItems,
+    GetItemById,
+    CreateItem,
+    UpdateItem,
+    DeleteItem
 }
--- server/routes/items.js
+++ server/routes/items.js
@@ -6,5 +6,9 @@ const controller = require('../controllers/ItemControllers')
 // Get /item
 
 router.get('/', controller.GetItems)
+router.get('/:id', controller.GetItemById)
+router.post('/', controller.CreateItem)
+router.put('/:id', controller.UpdateItem)
+router.delete('/:id', controller.DeleteItem)
 
 module.exports = router
