0159d4ef38fef46f7c8bd00f40b8e5b9f7c6ae57
Christina Jasik <cjjasik@gmail.com>
2022-11-04 16:47:58 -0400

    added code for tier 1 part 2

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -3,8 +3,14 @@ import React from 'react';
 export const Item = (props) => {
 
   return <>
-    <h3>{props.item.name}</h3>
-    <img src={props.item.image} alt={props.item.name} />
+    <div>
+      <h3 onClick = {() => props.onClick(props.item.id)}>{props.item.title}</h3>
+      <img src={props.item.image} alt={props.item.title} />
+    </div>
   </>
 } 
-	
+
+
+
+
+// onClick = {() => props.onClick(props.item.id)}
