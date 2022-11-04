5de72961895bf613a2805f22bf94825aee7c2303
marimart <marimart@penguin>
2022-11-04 15:36:05 -0400

    added create item test and delete item test

--- pratice.js
+++ pratice.js
@@ -6,8 +6,16 @@ describe('Item Model Tests', () => {
     beforeAll(async () => {
         await sequelize.sync({ force: true });
     });
-    test('can find Item', async () => {
+    test('can find item', async () => {
         const foundItem = await Items.findByPk(1)
         expect(foundItem.title).toEqual('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops')
     });
-    
+    test('can create item', async () => {
+        const createdItem = await Items.create(seedItems[0]) 
+        expect(createdItem.title).toBe(seedItems[0].title)
+    });
+    test('can delete an item', async () => {
+        const removedItem = await Items.destroy(seedItems[0])
+        expect(removedItem.title).toEqual('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops')
+    });
+})
