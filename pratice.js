a3f2314582f9b0d64bfa8c9e3dcb7f7792b36aa2
marimart <marimart@penguin>
2022-11-04 15:19:06 -0400

    starting on tests for item, added foundItem test

--- pratice.js
+++ pratice.js
@@ -1,1 +1,13 @@
-//hello there
+//tests for item 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+describe('Item Model Tests', () => {
+    beforeAll(async () => {
+        await sequelize.sync({ force: true });
+    });
+    test('can find Item', async () => {
+        const foundItem = await Items.findByPk(1)
+        expect(foundItem.title).toEqual('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops')
+    });
+    
