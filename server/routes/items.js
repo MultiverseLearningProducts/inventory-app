dabc15ac286287637d4f3097db54fc6845b76ce8
Brynner <bventura03@hotmail.com>
2022-11-04 15:34:44 -0400

    Took out Item from the routes folder

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,6 +1,5 @@
 const express = require("express")
 const router = express.Router()
-const { Item } = require('../models/index')
 const controller = require('../controllers/ItemControllers')
 
 // Get /item
