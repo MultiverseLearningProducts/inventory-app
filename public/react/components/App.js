df0d45049e2a658ad50a7dbd00f33439c38c3a9f
asianmichaelphelps <francischong@google.com>
2022-11-04 17:20:48 -0500

    render all items on frontend page

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { SaucesList } from './SaucesList';
+import { ItemsList } from './ItemsList';
+
+
 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
@@ -7,6 +10,7 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [sauces, setSauces] = useState([]);
+	const [items, setItems] = useState([]);
 
 	async function fetchSauces(){
 		try {
@@ -19,8 +23,20 @@ export const App = () => {
 		}
 	}
 
+	async function fetchItems(){
+		try {
+			const response = await fetch(`${apiURL}/items`);
+			const itemsData = await response.json();
+			
+			setItems(itemsData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+
 	useEffect(() => {
 		fetchSauces();
+		fetchItems();
 	}, []);
 
 	return (
@@ -33,6 +49,8 @@ export const App = () => {
 
 			<h2>All things 🔥</h2>
 			<SaucesList sauces={sauces} />
+			<ItemsList items={items} />
+
 		</main>
 	)
 }
