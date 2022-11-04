c9ad0cdfee9bf6d06fec84f1c42383fe19aa00d5
Emily Chieco <emilychieco@google.com>
2022-11-04 15:25:51 -0400

    Update Item sequelize model

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,21 @@
 const {Sequelize} = require('sequelize')
 const {sequelize} = require('../db')
 
-const Sauce = sequelize.define("sauces", {
+// initial model
+// const Sauce = sequelize.define("sauces", {
+//   name: Sequelize.STRING,
+//   image: Sequelize.STRING,
+// });
+
+const Item = sequelize.define("items", {
   name: Sequelize.STRING,
+  description: Sequelize.STRING,
+  price: Sequelize.NUMBER,
+  category: Sequelize.STRING,
   image: Sequelize.STRING,
 });
 
 module.exports = {
   db: sequelize,
-  Sauce,
+  Item,
 };
