265ff2feae84b0b26a55812c57b3bfe82aa2a6d1
marimart <marimart@penguin>
2022-11-07 13:06:13 -0500

    added find all items test

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -7,14 +7,19 @@ describe('Item Model Tests', () => {
     });
     test('can find item', async () => {
         const foundItem = await Item.findByPk(1)
-        expect(foundItem.title).toEqual('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops')
+        expect(foundItem.title).toEqual('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops');
     });
+    test('can find all items', async () => {
+        await Item.create(seedItem[0])
+        const findAllItems = await Item.findAll()
+        expect(findAllItems[0].price).toEqual(109.95);
+    })
     test('can create item', async () => {
         const createdItem = await Item.create(seedItems[0]) 
-        expect(createdItem.title).toBe(seedItems[0].title)
+        expect(createdItem.title).toBe(seedItems[0].title);
     });
     test('can delete an item', async () => {
         const removedItem = await Item.destroy(seedItems[0])
-        expect(removedItem.title).toEqual('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops')
+        expect(removedItem.title).toEqual('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops');
     });
 })
