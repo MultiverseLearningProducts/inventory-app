d5e175589f59d3d40a58df4eff977c1c54784c30
Stephanie <stephabdullah96@outlook.com>
2022-11-07 15:08:19 -0600

    correcting form input

--- public/react/components/Form.js
+++ public/react/components/Form.js
@@ -1,24 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { post } from "../../../server/routes";
 import apiURL from "../api"; 
 
 export const openForm = ({ props }) => {
     const fetchopenForm = async() => {
+    const [itemname, setItemname] = useState('');
     const response = await fetch (`${apiURL}/items`,{method:"Post"} );
     }
 
     return (
         <form onSubmit = {fetchopenForm}> 
-        <input/> 
-        "title":
-        <input/>
-        "price": 
-        <input/>
-        "description":
-        <input/>
-        "category":
-        <input/>
-        "image":
+        <input type = 'text' placeholder="title" value ={itemname}
+        onSubmit = {ev => setItemname(ev.target.value)}/> 
+        
+        <input  type = 'number' placeholder= "price" value ={price}
+        onSubmit = {ev => setItemname(ev.target.value)}/>
+        
+        <input  type = 'text' placeholder="description" value ={itemname}
+        onSubmit = {ev => setItemname(ev.target.value)}/>
+        
+        <input  type = 'text' placeholder = "category" value ={itemname}
+        onSubmit = {ev => setItemname(ev.target.value)}/>
+        
+        <input  type = 'text' placeholder = "image" value ={URL}
+        onSubmit = {ev => setItemname(ev.target.value)}/>
+        
         <button type = "submit">Submit</button>
         </form> 
     );
