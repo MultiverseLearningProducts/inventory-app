0cd46deb61da38f34a56b741577ef95bcf94d8ee
Brynner <bventura03@hotmail.com>
2022-11-07 11:42:15 -0500

    Deleted the sauce model

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,11 @@
 const {sequelize} = require('../db')
 
 const {Item} = require('./Item')
-const {Sauce} = require('./Sauce')
+
 
 // Relationships
 
 module.exports = {
   db: sequelize,
-  Sauce,
   Item
 };
