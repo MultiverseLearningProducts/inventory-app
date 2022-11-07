07fc1b54511e4e868e11e2f9918187e5061e54ca
marimart <marimart@penguin>
2022-11-07 12:52:37 -0500

    reorganized test folder structure

--- server/tests/index.test.js
+++ server/tests/index.test.js
@@ -1,4 +1,3 @@
-//tests for item 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
@@ -7,15 +6,15 @@ describe('Item Model Tests', () => {
         await sequelize.sync({ force: true });
     });
     test('can find item', async () => {
-        const foundItem = await Items.findByPk(1)
+        const foundItem = await Item.findByPk(1)
         expect(foundItem.title).toEqual('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops')
     });
     test('can create item', async () => {
-        const createdItem = await Items.create(seedItems[0]) 
+        const createdItem = await Item.create(seedItems[0]) 
         expect(createdItem.title).toBe(seedItems[0].title)
     });
     test('can delete an item', async () => {
-        const removedItem = await Items.destroy(seedItems[0])
+        const removedItem = await Item.destroy(seedItems[0])
         expect(removedItem.title).toEqual('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops')
     });
 })
