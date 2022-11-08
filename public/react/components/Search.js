6daf43862a47785eddcbd3ecffdbc0ec5f5d74b2
gpalencia <gpalencia@google.com>
2022-11-08 11:48:04 -0500

    created search compenent with conditional rendering

---
+++ public/react/components/Search.js
@@ -0,0 +1,49 @@
+import React from "react";
+import  { useState, useEffect } from "react";
+
+export const Search = ({fetchItems, items, setItems}) => {
+
+    const [categorySearch, setCategorySearch] = useState(false)
+    const [searching, setSearching] = useState(false)
+    
+    useEffect(() => {
+        fetchItems()
+    },[])
+
+    const getCategory = (category) => {
+        const categoryItem = items.filter(item => item.category === category)
+        setCategorySearch(categoryItem)
+        setSearching(!searching)
+    }
+
+
+    return <>   
+    <section className="searchBoxContainer">
+        <div>
+            <p>Men's Clothing</p>
+            <input onClick={() => {getCategory("Men's clothing");setSearching(!searching)}} type = 'checkbox'/>
+        </div>
+        <div>
+            <p>Women's Clothing</p>
+            <input onClick={() => {getCategory("Women's clothing");setSearching(!searching)}} type = 'checkbox'/>
+        </div>
+        <div>
+            <p>Jewelery</p>
+            <input onClick={() => {getCategory("Jewelery");setSearching(!searching)}} type = 'checkbox'/>
+        </div>
+        <div>
+            <p>Electronics</p>
+            <input onClick={() => {getCategory("Electronics");setSearching(!searching)}} type = 'checkbox'/>
+        </div>
+    </section>
+    <div className="searchResults">
+        {searching ? categorySearch.map(item => <>
+        <div key={item.id}>
+            <h3>{item.title}</h3>
+            <br/>
+            <img className="singleImg"src={item.image} alt={item.title}></img>
+        </div>
+        </>) : null}
+    </div>
+    </>
+}
