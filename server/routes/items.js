2224c6abf9b4eb1cf25d751b1477628962bea65f
Ange Batie <ange.batie@gmail.com>
2022-11-17 10:54:56 -0800

    started routes, removed all the sauces instances

--- server/routes/items.js
+++ server/routes/items.js
@@ -1,8 +1,11 @@
 const express = require("express");
-const router = express.Router();
+const seedData = require('../seedData.js')
 const { Item } = require("../models");
+const {check, validationResult} = require('express-validator')
 
-// GET /item
+const router = express.Router();
+
+// GET all items
 router.get("/", async (req, res, next) => {
   try {
     const items = await Item.findAll();
@@ -11,5 +14,42 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+// GET one item
+router.get('/:id', async (req, res) => {
+  const oneItem = await Item.findByPk(req.params.id)
+  res.json(oneItem)
+})
+
+// CREATE
+router.post('/', [
+  check('title').not().isEmpty().trim(),
+  check('price').not().isEmpty().trim(),
+  check('description').not().isEmpty().trim(),
+  check('category').not().isEmpty().trim(),
+  check('image').not().isEmpty().trim()
+], async (req, res) => {
+  const errors = validationResult(req)
+
+  if(!errors){
+    await Item.create(req.body)
+    let findAllItems = await Item.findAll()
+    res.json(findAllItems)
+  } else {
+    res.json({error: errors.array()})
+  }
+})
+
+// UPDATE
+router.put('/:id', async (req, res) => {
+  seedData[req.params.id - 1] = req.body
+  res.json(seedData)
+})
+
+// DELETE
+router.delete('/:id', async (req, res) => {
+  seedData.splice(req.params.id - 1, 1)
+  res.json(seedData)
+})
 
 module.exports = router;
