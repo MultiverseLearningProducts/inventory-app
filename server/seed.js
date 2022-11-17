5a0c5444cea1868dbd6b2fcbb5fb1f6ef5a889dc
ejero <rositaemakpo@gmail.com>
2022-11-16 18:44:08 -0800

    Corrected seed items map to display

--- server/seed.js
+++ server/seed.js
@@ -1,4 +1,4 @@
-const {sauces, items} = require('./seedData.js');
+const {items} = require('./seedData.js');
 
 const {sequelize} = require('./db');
 const {Item} = require('./models');
@@ -10,7 +10,7 @@ const seed = async () => {
         await sequelize.sync({ force: true });
     
         // insert data
-        await Promise.all(items.map(item => Item.create(items)));
+        await Promise.all(items.map(item => Item.create(item)));
 
         console.log("db populated!");
     } catch (error) {
