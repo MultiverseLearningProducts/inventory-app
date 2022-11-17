a8beddd71159c4c53a13914eaf52bd52e161daa2
Addy Rodriguez <rdz.33.a@gmail.com>
2022-11-17 15:23:22 -0600

    Front end view for all items

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -3,7 +3,11 @@ import React from 'react';
 export const Item = (props) => {
 
   return <>
-    <h3>{props.item.title}</h3>
+    <h3>Item: {props.item.title}</h3>
+    <h3>Price: {props.item.price}</h3>
+    <h3>Description: {props.item.description}</h3>    
+    <h3>Category: {props.item.category}</h3>      
+    
     <img src={props.item.image} alt={props.item.name} />
   </>
 } 
