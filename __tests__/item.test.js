1982cebe7b2986d93e9759ed2cf3b327838ce890
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-11-18 13:24:36 -0800

    deleting file to rename

--- __tests__/item.test.js
+++
@@ -1,7 +0,0 @@
-const { Item, sequelize } = require('../server/models/index.js');
-
-describe('------ testing Item calls ------', () => {
-    beforeAll(async () => {
-        await sequelize.sync({ force: true });
-    });
-});
