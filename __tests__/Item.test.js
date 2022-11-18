e1bce1d747bda3022da16964a7506447892eff9d
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-11-18 13:25:19 -0800

    readding file with same content

---
+++ __tests__/Item.test.js
@@ -0,0 +1,9 @@
+const { Item, sequelize } = require('../server/models/index.js');
+
+describe('------ testing Item calls ------', () => {
+    beforeAll(async () => {
+        await sequelize.sync({ force: true });
+    });
+
+    
+});
