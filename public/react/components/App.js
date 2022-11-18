5c3e31c108e4539577bcabecce40bbe4843d0793
Halina Cox <halina.cox@halina.cox-XPHWNQNK73>
2022-11-18 10:24:28 -0800

    single item

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -7,13 +7,26 @@ import apiURL from '../api';
 export const App = () => {
 
 	const [items, setItems] = useState([]);
+	const [itemDetail, setItemDetail] = useState(null);
 
+	//all items
 	async function fetchItems(){
 		try {
 			const response = await fetch(`${apiURL}/items`);
 			const itemsData = await response.json();
 			
 			setItems(itemsData);
+		} catch (err) {
+			console.log("Oh no an error! ", err)
+		}
+	}
+	//single item
+	async function fetchSingleItems(){
+		try {
+			const response = await fetch(`${apiURL}/items/${id}`);
+			const singleItemsData = await response.json();
+			
+			setItems(singleItemsData);
 		} catch (err) {
 			console.log("Oh no an error! ", err)
 		}
