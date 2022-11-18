473f7f73c874f8ce078b4a44874d36353a6a722c
Deja Clarke (dejclark) <dejclark@cisco.com>
2022-11-18 14:06:29 -0800

    fetch one item

--- public/react/components/ItemsList.js
+++ public/react/components/ItemsList.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { Item } from './Item';
 
-export const ItemsList = ({items, fetchItems}) => {
+export const ItemsList = ({items, fetchItems, setSingleItemId, setSingleView, singleItemId, singleView}) => {
 	return <>
 		{
 			items.map((item, idx) => {
-				return <Item item={item} key={idx} fetchItems ={fetchItems}/>
+				return (singleView ? (
+					(singleItemId === item.id) ?
+						(<Item item={item} key={idx} fetchItems ={fetchItems} singleItemId={singleItemId} singleView={singleView} 
+						setSingleItemId={setSingleItemId} setSingleView={setSingleView}  />):null
+				):(
+					<Item item={item} key={idx} fetchItems ={fetchItems} singleItemId={singleItemId} singleView={singleView} 
+				setSingleItemId={setSingleItemId} setSingleView={setSingleView}  />
+				))
 			})
 		}
 	</>
