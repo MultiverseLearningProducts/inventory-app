797ceec27e9d779665199a93f819a00556f031e2
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-11-18 15:43:34 -0800

    added coverage folder to gitignore

--- __tests__/Item.test.js
+++ __tests__/Item.test.js
@@ -17,6 +17,10 @@ describe('------ testing Item calls ------', () => {
              
         });
 
-        
+        expect(backpack['title']).toBe('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops');
+        expect(backpack['price']).toBe(109.95);
+        expect(backpack['description']).toBe('Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday');
+        expect(backpack['category']).toBe('men\'s clothing');
+        expect(backpack['image']).toBe('https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg');
     });
 });
