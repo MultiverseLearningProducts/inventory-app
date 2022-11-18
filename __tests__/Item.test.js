84660630119d94e0fee5f08361f79e9793e56a52
Dar-Ci Calhoun <darci.calhoun@gmail.com>
2022-11-18 13:29:06 -0800

    writing Item.create() test, created seed variable

--- __tests__/Item.test.js
+++ __tests__/Item.test.js
@@ -5,5 +5,18 @@ describe('------ testing Item calls ------', () => {
         await sequelize.sync({ force: true });
     });
 
-    
+    it('can add an Item record to the database', async () => {
+
+        const backpack = await Item.create({
+            
+            "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+            "price":109.95,
+            "description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+            "category":"men's clothing",
+            "image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+             
+        });
+
+        
+    });
 });
