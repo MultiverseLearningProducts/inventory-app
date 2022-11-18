5e241db21319bc8672730d8b8386797d1ba7707e
davelarde <danielavelarde4@gmail.com>
2022-11-18 17:54:40 -0500

    Render details and add back button

--- public/react/components/Item.js
+++ public/react/components/Item.js
@@ -1,7 +1,7 @@
-import React from 'react';
-
+import React , {useState} from 'react';
+import apiURL from '../api';
 export const Item = (props) => {
-
+const [details, setDetails] = useState(false)
   const handleSingleItem = () => {
     props.setSingleView(!props.singleView)
     if(props.singleItemId === 0) {
@@ -10,11 +10,33 @@ export const Item = (props) => {
       props.setSingleItemId(0)
     }
   }
-
+  const userClick = async(props) =>{
+    const res = await fetch(`${apiURL}/items/${props.item.id}`)
+    const dataDetails = await res.json()
+    console.log(dataDetails)
+    setDetails(dataDetails)
+  }
   return <>
   <div onClick={handleSingleItem}>
       <h3 >{props.item.title}</h3>
       <img src={props.item.image} alt={props.item.title} />
+    </div>
+    <div>
+    {details ?
+  <>
+    <h3>{props.item.title}</h3>
+    <h4> Price:</h4><p>{details.price}</p>
+    <h4> Description:</h4><p>{details.description}</p>
+    <button onClick ={handleSingleItem}>Back to main list</button>
+  </>
+  :
+  <div>
+  <h3>{props.item.title}</h3>
+  <button onClick = {() => userClick(props)}> Details</button>
+</div>
+
+  }
+
     </div>
   </>
 } 
