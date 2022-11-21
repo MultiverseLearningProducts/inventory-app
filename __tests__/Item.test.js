3e8b94569b17646e9f66634b76bc368c752894f0
Krystal <lopezkrystal10@gmail.com>
2022-11-20 16:52:19 -0800

    created test for post route, resolved regenerator-runtime error

--- __tests__/Item.test.js
+++ __tests__/Item.test.js
@@ -1,4 +1,6 @@
 const { Item, sequelize } = require('../server/models/index.js');
+const { items } = require("../server/seedData");
+const runtime = require("regenerator-runtime");
 
 describe('------ testing Item calls ------', () => {
     beforeAll(async () => {
@@ -7,20 +9,14 @@ describe('------ testing Item calls ------', () => {
 
     it('can add an Item record to the database', async () => {
 
-        const backpack = await Item.create({
-            
-            "title":"Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
-            "price":109.95,
-            "description":"Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
-            "category":"men's clothing",
-            "image":"https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
-             
-        });
+        const backpack = await Item.create(items[0]);
 
-        expect(backpack['title']).toBe('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops');
-        expect(backpack['price']).toBe(109.95);
-        expect(backpack['description']).toBe('Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday');
-        expect(backpack['category']).toBe('men\'s clothing');
-        expect(backpack['image']).toBe('https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg');
+        expect(backpack.title).toBe('Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops');
+        expect(backpack.price).toBe(109.95);
+        expect(backpack.description).toBe('Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday');
+        expect(backpack.category).toBe('men\'s clothing');
+        expect(backpack.image).toBe('https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg');
     });
+
+
 });
