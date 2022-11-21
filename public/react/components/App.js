c886a5e917ae43132409a23d24f9ae4d913f1d69
Krystal <lopezkrystal10@gmail.com>
2022-11-20 17:15:24 -0800

    commit so that I can pull down from dev

--- public/react/components/App.js
+++ public/react/components/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { ItemsList } from './ItemsList';
-
+ 
 // import and prepend the api url to any fetch calls
 import apiURL from '../api';
 
