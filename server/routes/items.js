085d1c9a5afe0e90a24998baaa19ce1b65409ee1
drespana <drespana@google.com>
2023-10-30 10:55:03 -0500

    PUT, POST, and DELETE requests added for Items

--- server/routes/items.js
+++ server/routes/items.js
@@ -6,16 +6,15 @@ const itemRouter = Router();
 // GET /item
 itemRouter.get("/", async (req, res, next) => {
   try {
-    // might need res.json() for the info to be parsed into JSON
     const items = await Item.findAll();
-    res.send(items);
+    res.json(items);
   } catch (error) {
     next(error);
   }
 });
 
 // GET item/:id
-itemRouter.get('/:id', async (req, res, next) => {
+itemRouter.get("/:id", async (req, res, next) => {
   try {
       const found = await Item.findByPk(req.params.id);
       res.json(found)
@@ -23,5 +22,39 @@ itemRouter.get('/:id', async (req, res, next) => {
       next(err)
   }
 })
+
+// POST new Item
+itemRouter.post("/", async (req, res, next) => {
+  try {
+    await Item.create(req.body)
+    const all = await Item.findAll()
+    res.json(all)
+  } catch (err){
+    next(err)
+  }
+})
+
+// PUT update Item
+itemRouter.put("/:id", async (req,res,next) => {
+  try{
+    await Item.update(req.body, {where:{id:req.params.id}})
+    const find = await Item.findByPk(req.params.id)
+    res.json(find)
+  } catch (err){
+    next(err)
+  }
+})
+
+// DELETE Item by ID
+itemRouter.delete("/:id", async(req, res, next) => {
+  try{
+    await Item.destroy({where:{id:req.params.id}})
+    const find = await Item.findAll()
+    res.json(find)
+  } catch (err){
+    next(err)
+  }
+})
+
 
 module.exports = itemRouter;
