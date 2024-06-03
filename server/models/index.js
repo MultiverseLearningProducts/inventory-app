cada47cc82fc1dac0df484811751570bcc7d928b
littlegaz <gareth.t.horne@hotmail.co.uk>
2024-06-03 12:41:48 +0100

    item added to models index

--- server/models/index.js
+++ server/models/index.js
@@ -5,8 +5,18 @@ const Sauce = sequelize.define("sauces", {
   name: Sequelize.STRING,
   image: Sequelize.STRING,
 });
+// at some point we will need to remove the sauce as this is not needed
+
+const Item = sequelize.define("item", {
+  name: Sequelize.STRING,
+  description: Sequelize.STRING,
+  price: Sequelize.DOUBLE, //check if float or double is correct for numbers
+  catagory: Sequelize.STRING,
+  image: Sequelize.STRING,
+});
 
 module.exports = {
   db: sequelize,
   Sauce,
+  Item,
 };
