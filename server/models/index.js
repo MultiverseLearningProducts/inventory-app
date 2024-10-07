f29d52f56f3e2412658f2d6368b42ecea6823ed8
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2024-10-07 15:56:07 -0400

    Created fetch for Items Api,files names changed accordingly to Items data

--- server/models/index.js
+++ server/models/index.js
@@ -1,12 +1,12 @@
-const {Sequelize} = require('sequelize')
-const {sequelize} = require('../db')
+const { Sequelize } = require("sequelize");
+const { sequelize } = require("../db");
 
-const Sauce = sequelize.define("sauces", {
+const Items = sequelize.define("items", {
   name: Sequelize.STRING,
   image: Sequelize.STRING,
 });
 
 module.exports = {
   db: sequelize,
-  Sauce,
+  Items,
 };
