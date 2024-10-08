b452f9b095241462d7c044fb9dd879fb51ffa0b0
Lilit Sanjyan <133781980+LSanjyan@users.noreply.github.com>
2024-10-08 13:05:50 -0400

    Update and delete routes created.

--- server/routes/items.js
+++ server/routes/items.js
@@ -2,30 +2,51 @@ const express = require("express");
 const router = express.Router();
 const { Item } = require("../models/Item");
 
-
 // get /items returns all item instances
 router.get("/", async (req, res, next) => {
-    try {
-      const items = await Item.findAll();
-      res.send(items);
-    } catch (e) {
-      next(e);
-    }
-  });
+  try {
+    const items = await Item.findAll();
+    res.send(items);
+  } catch (e) {
+    next(e);
+  }
+});
 
-  router.get("/:itemId", async (req, res, next) => {
-    try {
-        const item = await Item.findByPk(req.params.itemId)
-    
-        if (!item) {
-          res.status(404)
-          next()
-        } else {
-          res.send(item)
-        }
-      } catch (e) {
-        next(e)
-      }
-    })
+router.get("/:itemId", async (req, res, next) => {
+  try {
+    const item = await Item.findByPk(req.params.itemId);
+
+    if (!item) {
+      res.status(404);
+      next();
+    } else {
+      res.send(item);
+    }
+  } catch (e) {
+    next(e);
+  }
+});
+//Update route
+router.put("/:itemId", async (req, res, next) => {
+  try {
+    const item = await Item.findByPk(req.params.itemId);
+    if (!item) return res.status(404).send("Item not found");
+
+    const updatedItem = await item.update(req.body);
+    res.send(updatedItem);
+  } catch (e) {
+    next(e);
+  }
+});
+//Delete route
+router.delete("/:itemId", async (req, res, next) => {
+  try {
+    const deleted = await Item.destroy({ where: { id: req.params.itemId } });
+    if (!deleted) return res.status(404).send("Item not found");
+    res.status(204).send();
+  } catch (e) {
+    next(e);
+  }
+});
 
 module.exports = router;
